b64f14d53a9cae9901e2e6148cd4b6deb4435446
quaresmina <arielquaresma90@gmail.com>
2022-05-30 17:43:54 -0300

    Teste da calculadora nova

---
+++ src/calculadora/calculadora.java
@@ -0,0 +1,14 @@
+package calculadora;
+
+public class calculadora {
+    public static void main(String[] args) {
+
+    }
+
+    public static int somar(int a, int b){
+        return a + b;
+    }
+    public static int subtrair(int a){
+        return a - b;
+    }
+}
